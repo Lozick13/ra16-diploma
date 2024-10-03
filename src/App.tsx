92b8d640a9c9d298b23d2bd51b89dbda82abb7cf
Lozick <dlozichnyy2005@gmail.com>
2024-10-03 11:52:41 +0700

    routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,25 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import CartPage from './pages/CartPage/CartPage';
+import CatalogPage from './pages/CatalogPage/CatalogPage';
+import InfoPage from './pages/InfoPage/InfoPage';
+import MainPage from './pages/MainPage/MainPage';
+import NotFoundPage from './pages/NotFoundPage/NotFoundPage';
+import ProductPage from './pages/ProductPage/ProductPage';
 
 function App() {
-	return <></>;
+	return (
+		<>
+			<Routes>
+				<Route path='/' index element={<MainPage />} />
+				<Route path='/catalog' element={<CatalogPage />} />
+				<Route path='/info' element={<InfoPage />} />
+				<Route path='/product/:id' element={<ProductPage />} />
+				<Route path='/cart' element={<CartPage />} />
+				<Route path='*' element={<NotFoundPage />} />
+			</Routes>
+		</>
+	);
 }
 
 export default App;
