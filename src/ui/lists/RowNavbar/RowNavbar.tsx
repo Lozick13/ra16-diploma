3ce9b3e2cfa7bac44bbaf866559877b7657c9de2
Lozick <dlozichnyy2005@gmail.com>
2024-10-05 12:07:03 +0700

    RowNavbar

---
+++ src/ui/lists/RowNavbar/RowNavbar.tsx
@@ -0,0 +1,25 @@
+import { FC } from 'react';
+import { Link } from 'react-router-dom';
+
+const RowNavbar: FC<{
+	items: { text: string; link: string; active?: boolean }[];
+}> = ({ items }) => {
+	return (
+		<>
+			<ul className='navbar-nav mr-auto'>
+				{items.map(item => (
+					<li
+						key={item.link}
+						className={`nav-item ${item.active ? 'active' : ''}`}
+					>
+						<Link className='nav-link' to={item.link}>
+							{item.text}
+						</Link>
+					</li>
+				))}
+			</ul>
+		</>
+	);
+};
+
+export default RowNavbar;
---
+++ src/ui/lists/RowNavbar/index.ts
@@ -0,0 +1,1 @@
+export * from './RowNavbar.tsx';
