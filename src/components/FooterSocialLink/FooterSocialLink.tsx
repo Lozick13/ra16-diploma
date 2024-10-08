aa874980b3747c8409a2e36c00734820a0563f36
Lozick <dlozichnyy2005@gmail.com>
2024-10-08 07:41:55 +0700

    FooterSocialLink

---
+++ src/components/FooterSocialLink/FooterSocialLink.tsx
@@ -0,0 +1,19 @@
+import { FC } from 'react';
+import './footersociallink.css';
+
+const FooterSocialLink: FC<{ items: string[] }> = ({ items }) => {
+	return (
+		<>
+			<div className='footer-social-links'>
+				{items.map((item, index) => (
+					<div
+						key={item + '-' + index}
+						className={`footer-social-link footer-social-link-${item}`}
+					/>
+				))}
+			</div>
+		</>
+	);
+};
+
+export default FooterSocialLink;
