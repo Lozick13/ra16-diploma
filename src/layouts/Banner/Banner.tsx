264bf8d4ba4d8b95d72720845cf7f5b0a4601416
Lozick <dlozichnyy2005@gmail.com>
2024-10-08 07:47:16 +0700

    Banner

---
+++ src/layouts/Banner/Banner.tsx
@@ -0,0 +1,18 @@
+import './banner.css';
+
+const Banner = () => {
+	return (
+		<>
+			<div className='banner'>
+				<img
+					src='./assets/banner.jpg'
+					className='img-fluid'
+					alt='К весне готовы!'
+				/>
+				<h2 className='banner-header'>К весне готовы!</h2>
+			</div>
+		</>
+	);
+};
+
+export default Banner;
