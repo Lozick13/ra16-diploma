ec2cc43a5d6d82a4a88bbbe16f9f58a91e2536f5
Lozick <dlozichnyy2005@gmail.com>
2024-10-08 07:37:52 +0700

    FooterPay

---
+++ src/components/FooterPay/FooterPay.tsx
@@ -0,0 +1,19 @@
+import { FC } from 'react';
+import './footerpay.css';
+
+const FooterPay: FC<{ items: string[] }> = ({ items }) => {
+	return (
+		<>
+			<div className='footer-pay'>
+				{items.map((item, index) => (
+					<div
+						key={item + '-' + index}
+						className={`footer-pay-systems footer-pay-systems-${item}`}
+					/>
+				))}
+			</div>
+		</>
+	);
+};
+
+export default FooterPay;
