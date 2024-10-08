980a5a71b9e606e1eba01b7375c541acc4fc801b
Lozick <dlozichnyy2005@gmail.com>
2024-10-08 07:47:47 +0700

    NotFoundPage

--- src/pages/NotFoundPage/NotFoundPage.tsx
+++ src/pages/NotFoundPage/NotFoundPage.tsx
@@ -1,11 +1,22 @@
-import classes from './notfoundpage.module.css';
+import Banner from '../../layouts/Banner/Banner';
+import './notfoundpage.css';
 
 const NotFoundPage = () => {
-return (
-<>
-
-</>
-);
+	return (
+		<>
+			<main className='container'>
+				<div className='row'>
+					<div className='col'>
+						<Banner/>
+						<section className='top-sales'>
+							<h2 className='text-center'>Страница не найдена</h2>
+							<p>Извините, такая страница не найдена!</p>
+						</section>
+					</div>
+				</div>
+			</main>
+		</>
+	);
 };
 
 export default NotFoundPage;
