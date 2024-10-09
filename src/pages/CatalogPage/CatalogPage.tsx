6ad70d5a2401959ca9321252114f975c86ba5d02
Lozick <dlozichnyy2005@gmail.com>
2024-10-09 10:05:25 +0700

    add catalog to catalog page

--- src/pages/CatalogPage/CatalogPage.tsx
+++ src/pages/CatalogPage/CatalogPage.tsx
@@ -1,11 +1,18 @@
+import Catalog from '../../components/Catalog/Catalog';
 import './catalogpage.css';
 
 const CatalogPage = () => {
-return (
-<>
-
-</>
-);
+	return (
+		<>
+			<main className='container'>
+				<div className='row'>
+					<div className='col'>
+						<Catalog />
+					</div>
+				</div>
+			</main>
+		</>
+	);
 };
 
 export default CatalogPage;
