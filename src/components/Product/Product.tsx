51e8eca6885f8e666625989338c391e894cf191b
Lozick <dlozichnyy2005@gmail.com>
2024-10-09 09:59:50 +0700

    improving product design

--- src/components/Product/Product.tsx
+++ src/components/Product/Product.tsx
@@ -13,8 +13,8 @@ const Product: FC<{
 		<>
 			<div className='col-4'>
 				<div className={`card ${catalog ? 'catalog-item-card' : ''}`}>
-					<img src={url} className='card-img-top img-fluid' alt={title} />
-					<div className='card-body'>
+					<img src={url} className='card-img-top img-fluid card-img-wrap' alt={title} />
+					<div className='card-body card-body-wrap'>
 						<p className='card-text'>{title}</p>
 						<p className='card-text'>{price} руб.</p>
 						<Link to={`/products/${id}`} className='btn btn-outline-primary'>
