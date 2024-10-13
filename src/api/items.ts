0a250cb5ed27ba78594cf9d89597f67e73705d07
Lozick <dlozichnyy2005@gmail.com>
2024-10-13 11:19:05 +0700

    items query aggregation

--- src/api/items.ts
+++ src/api/items.ts
@@ -1,25 +1,15 @@
-export const fetchItems = async () => {
-	const response = await fetch('http://localhost:7070/api/items');
+export const fetchItems = async (params?: {
+	categoryId?: number;
+	offset?: number;
+}) => {
+	const URLParams = new URLSearchParams();
+	if (params?.categoryId)
+		URLParams.append('categoryId', params.categoryId.toString());
+	if (params?.offset) URLParams.append('offset', params.offset.toString());
+
+	const response = await fetch(`http://localhost:7070/api/items?${URLParams}`);
 	if (!response.ok) {
 		throw new Error(response.statusText);
-	}
-	return await response.json();
-};
-
-export const fetchItemsByCategories = async (id: string) => {
-	const params = new URLSearchParams({ categoryId: id });
-
-	const response = await fetch(`http://localhost:7070/api/items?${params}`);
-	if (!response.ok) {
-		throw new Error(response.statusText);
-	}
-	return await response.json();
-};
-
-export const fetchItemsWithOffset = async (offset: number = 6) => {
-	const response = await fetch(`http://localhost:7070/api/items?offset=${offset}`);
-	if (!response.ok) {
-			throw new Error(response.statusText);
 	}
 	return await response.json();
 };
