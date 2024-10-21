a827759ae2a5f067f46c87e33c9daf89dba7cd1d
Lozick <dlozichnyy2005@gmail.com>
2024-10-21 17:42:47 +0700

    adding the quest parameter to the itemReducer

--- src/api/items.ts
+++ src/api/items.ts
@@ -1,17 +1,18 @@
 import { URL } from './url';
 
 export const fetchItems = async (params?: {
-	categoryId?: number;
-	offset?: number;
+  categoryId?: number;
+  offset?: number;
+  quest?: string;
 }) => {
-	const URLParams = new URLSearchParams();
-	if (params?.categoryId)
-		URLParams.append('categoryId', params.categoryId.toString());
-	if (params?.offset) URLParams.append('offset', params.offset.toString());
+  const URLParams = new URLSearchParams();
+  if (params?.categoryId) URLParams.append('categoryId', params.categoryId.toString());
+  if (params?.offset) URLParams.append('offset', params.offset.toString());
+  if (params?.quest) URLParams.append('q', params.quest.toString());
 
-	const response = await fetch(`${URL}/items?${URLParams}`);
-	if (!response.ok) {
-		throw new Error(response.statusText);
-	}
-	return await response.json();
+  const response = await fetch(`${URL}/items?${URLParams}`);
+  if (!response.ok) {
+    throw new Error(response.statusText);
+  }
+  return await response.json();
 };
--- src/redux/saga/ItemsSaga.ts
+++ src/redux/saga/ItemsSaga.ts
@@ -3,45 +3,46 @@ import { put, retry, takeLatest } from 'redux-saga/effects';
 import { fetchItems } from '../../api';
 import { Item } from '../../api/types';
 import {
-	addItems,
-	fetchItemsFailure,
-	fetchItemsRequest,
-	fetchItemsSuccess,
-	setItems,
+  addItems,
+  fetchItemsFailure,
+  fetchItemsRequest,
+  fetchItemsSuccess,
+  setItems,
 } from '../slices/itemsSlice';
 
 const retryCount = 2;
 const retryDelay = 500;
 
 function* workerFetchItemsRequest(
-	action?: PayloadAction<{ categoryId?: number; offset?: number }>
+  action?: PayloadAction<{ categoryId?: number; offset?: number; quest?: string }>,
 ) {
-	try {
-		const data: Item[] = yield retry(retryCount, retryDelay, fetchItems, {
-			categoryId: action?.payload.categoryId,
-			offset: action?.payload.offset,
-		});
+  try {
+    const data: Item[] = yield retry(retryCount, retryDelay, fetchItems, {
+      categoryId: action?.payload.categoryId,
+      offset: action?.payload.offset,
+      quest: action?.payload.quest,
+    });
 
-		if (action?.payload.offset) {
-			yield put(addItems(data));
-			yield put(fetchItemsSuccess(data.length));
-		} else {
-			yield put(setItems(data));
-			yield put(fetchItemsSuccess());
-		}
-	} catch (error: unknown) {
-		let errorMessage: string;
+    if (action?.payload.offset) {
+      yield put(addItems(data));
+      yield put(fetchItemsSuccess(data.length));
+    } else {
+      yield put(setItems(data));
+      yield put(fetchItemsSuccess());
+    }
+  } catch (error: unknown) {
+    let errorMessage: string;
 
-		if (error instanceof Error) {
-			errorMessage = error.message;
-		} else {
-			errorMessage = 'Unknown error';
-		}
+    if (error instanceof Error) {
+      errorMessage = error.message;
+    } else {
+      errorMessage = 'Unknown error';
+    }
 
-		yield put(fetchItemsFailure(errorMessage));
-	}
+    yield put(fetchItemsFailure(errorMessage));
+  }
 }
 
 export function* watchItemsSaga() {
-	yield takeLatest(fetchItemsRequest, workerFetchItemsRequest);
+  yield takeLatest(fetchItemsRequest, workerFetchItemsRequest);
 }
